991898c92424c50213f064e8a47639624131e8cb
krispo <konstantin.skipor@gmail.com>
2013-08-24 18:01:50 +0400

    added angular-seed-play app

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,11 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm)
+    anorm,
+    // Adding WebJars dependencies
+    "org.webjars" % "angularjs" % "1.1.5-1",
+    "org.webjars" % "requirejs" % "2.1.8",
+    "org.webjars" %% "webjars-play" % "2.1.0-3")
 
   val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here      
     playAssetsDirectories <+= baseDirectory / "demos")
