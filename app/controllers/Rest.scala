5377369ef7f4e9bebb755c73e6bcdd0bd02fc545
krispo <konstantin.skipor@gmail.com>
2013-09-18 11:00:02 +0400

    mongo

--- app/controllers/Rest.scala
+++ app/controllers/Rest.scala
@@ -2,12 +2,9 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import play.api.libs.json._
 
 object Rest extends Controller {
-  
-  def getData = Action {    
-    Ok("This is the data from server!")
+  def getData = Action {
+    Ok("Data from server!")
   }
-  
 }
--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,9 @@ object ApplicationBuild extends Build {
     // Adding WebJars dependencies
     "org.webjars" % "angularjs" % "1.1.5-1",
     "org.webjars" % "requirejs" % "2.1.8",
-    "org.webjars" %% "webjars-play" % "2.1.0-3")
+    "org.webjars" %% "webjars-play" % "2.1.0-3",
+    // Adding MongoDB dependencies
+    "org.reactivemongo" %% "play2-reactivemongo" % "0.9")
 
   val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here      
     playAssetsDirectories <+= baseDirectory / "demos")
